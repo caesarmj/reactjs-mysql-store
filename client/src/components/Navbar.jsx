258e00d745d64aa5a69f97e87134873f8ba262c9
caesarmj <caesariomj7@gmail.com>
2023-05-04 15:21:33 +0700

    Developing home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { ReactComponent as Phone } from "../assets/images/phone.svg";
 import { ReactComponent as Menu } from "../assets/images/menu.svg";
 import Logo from "../assets/images/logo-50.webp";
 import { ReactComponent as User } from "../assets/images/user.svg";
@@ -10,66 +9,42 @@ import { ReactComponent as Cart } from "../assets/images/shopping-bag.svg";
 const Navbar = () => {
   return (
     <header>
-      <nav className="container navbar-small">
-        <p>Dapatkan Diskon Menarik dan Promo Spesial Hanya di Sini!</p>
-        <ul>
+      <nav className="container">
+        <a href="/" className="logo">
+          <img src={Logo} alt="Logo Jogja Electrik" />
+          <h4>JOGJA ELECTRIK</h4>
+        </a>
+        <ul className="links">
           <li>
-            <Link to="/login">Masuk</Link>
+            <a href="/">Beranda</a>
           </li>
           <li>
-            <Link to="/registration">Registrasi</Link>
+            <a href="/products">Produk</a>
           </li>
           <li>
-            <Link to="/contact-us">Kontak Kami</Link>
-          </li>
-          <li className="divider">|</li>
-          <li className="social facebook">
-            <Link to="">FB</Link>
-          </li>
-          <li className="social instagram">
-            <Link to="">IG</Link>
-          </li>
-          <li className="social whatsapp">
-            <Link to="">WA</Link>
+            <a href="/categories">Kategori</a>
           </li>
         </ul>
-      </nav>
-      <nav className="container navbar-large">
-        <div className="call-wrapper">
-          <Phone className="call-logo" />
-          <div className="call-info">
-            <h6>Call Us</h6>
-            <p>+62 123 456 789</p>
-          </div>
-        </div>
-        <button className="container menu-toggle">
-          <Menu className="menu-icon" />
-        </button>
-        <div className="logo-wrapper">
-          <img src={Logo} alt="" />
-          <h6 className="container menu-heading">JOGJA ELECTRIK</h6>
-        </div>
-        <ul className="navbar-large-menu">
-          <li>
-            <User />
-          </li>
-          <li>
-            <Heart />
-          </li>
+        <ul className="user-menu">
           <li>
             <Search />
           </li>
           <li>
-            <Cart />
+            <Heart />
           </li>
-        </ul>
-      </nav>
-      <nav className="container navbar-menu">
-        <ul>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
+          <li className="authentication">
+            <a href="/login">Masuk</a>
+          </li>
+          <li className="authentication">
+            <a href="/registration" className="btn btn-outline">
+              Daftar
+            </a>
+          </li>
+          <li className="menu-toggle">
+            <button>
+              <Menu />
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -1,7 +1,16 @@
 const Home = () => {
   return (
     <main>
-      <div className="container">Home</div>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Title</h1>
+          <p>Subtitle</p>
+          <a href="" className="btn btn-primary">
+            Tombol
+          </a>
+        </div>
+        <img src="http://via.placeholder.com/1920x1080" alt="Hero 1" />
+      </section>
     </main>
   );
 };
