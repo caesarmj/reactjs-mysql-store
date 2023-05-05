20ca933a0b74ed12548b5ed3271a60f6cce78346
caesarmj <caesariomj7@gmail.com>
2023-05-05 22:23:51 +0700

    Developing section inside  home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,64 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="container">Footer</div>
+      <div className="container">
+        <ul>
+          <li>
+            <a href="#">Header Menu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Header Menu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Header Menu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Header Menu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+          <li>
+            <a href="#">Submenu</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -1,3 +1,5 @@
+import { ReactComponent as Arrow } from "../../assets/images/arrow-right.svg";
+
 const Home = () => {
   return (
     <main>
@@ -11,6 +13,47 @@ const Home = () => {
         </div>
         <img src="http://via.placeholder.com/1920x1080" alt="Hero 1" />
       </section>
+      <section className="container categories">
+        <h2 className="section-header">Category Section</h2>
+        <div className="category-wrapper">
+          <div className="category-card">
+            <img src="http://via.placeholder.com/1000" alt="Category Image" />
+            <div className="category-content">
+              <div className="category-content-heading">
+                <h4>Nama Kategori</h4>
+                <p>24 Produk</p>
+              </div>
+              <div className="category-content-button">
+                <a href="#">
+                  <Arrow className="arrow-button" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="category-card">
+            <img src="http://via.placeholder.com/1000" alt="Category Image" />
+            <div className="category-content">
+              <div className="category-content-heading">
+                <h4>Nama Kategori</h4>
+                <p>24 Produk</p>
+              </div>
+              <div className="category-content-button">
+                <a href="#">
+                  <Arrow className="arrow-button" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="promo-card">
+            <div className="promo-content">
+              <h4>Promo</h4>
+              <p>Paragraf</p>
+              <button>Tombol</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="container featured">Featured</section>
     </main>
   );
 };
